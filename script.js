9bc43ffaa295472d0f8ef9e0afcfcb21c2ea82d4
Joéllie Sprumont <joellie@siriusschool.be>
2020-07-30 13:32:38 +0200

    update-scroll

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() > 200) {
+    $('.landing__contactBtn').addClass('transparent');
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $('.landing__contactBtn').removeClass('transparent');
+  }
+});
