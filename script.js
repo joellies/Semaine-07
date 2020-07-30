e838dc885d168cc8b18c5f7a42eb6c102b162beb
Joéllie Sprumont <joellie@siriusschool.be>
2020-07-30 16:31:46 +0200

    profile-page-ok

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
-
-$(window).on('scroll', function () {
-  if ($(window).scrollTop() > 200) {
-    $('.landing__contactBtn').addClass('transparent');
+/*window.on('scroll', function () {
+  if (window.scrollTop() > 200) {
+    '.landing__contactBtn'.addClass('transparent');
   } else {
     //remove the background property so it comes transparent again (defined in your css)
-    $('.landing__contactBtn').removeClass('transparent');
+    '.landing__contactBtn'.removeClass('transparent');
   }
 });
+*/
